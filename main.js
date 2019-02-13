730fd1ea55139f33323664e9ff14ff81df26afeb
Neddam <0564873@students.dadeschools.net>
2019-02-13 14:03:52 -0500

    started car game project

---
+++ main.js
@@ -0,0 +1,23 @@
+// VARIABLES 
+const car = {
+    make: 'Coccinelle',
+    model: 'Rodster',
+    color: 'Orange',
+    mileage: '0'
+    isWorking: true,
+
+    driveTowork(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage = this.mileage + 10;
+        alert(`New Mileage: ${this.mileage}`);
+
+    
+    
+    }
+
+}
+
+//FUNCTIONS 
+//Legs all of our car's current stats to the console
+/*
+
